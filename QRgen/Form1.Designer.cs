f2b7716b223fb33f284d36bfce16a48fe82c0a19
garnerm91 <garnerm91@gmail.com>
2024-06-17 20:25:53 -0400

    Add project files.

---
+++ QRgen/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace QRgen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.pictureBoxQRCode = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(572, 431);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "Make";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(12, 408);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxInput.Size = new System.Drawing.Size(554, 46);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // pictureBoxQRCode
+            // 
+            this.pictureBoxQRCode.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxQRCode.Name = "pictureBoxQRCode";
+            this.pictureBoxQRCode.Size = new System.Drawing.Size(554, 390);
+            this.pictureBoxQRCode.TabIndex = 2;
+            this.pictureBoxQRCode.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(572, 380);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(179, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(600, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "QR Code Size (Pixles Per bit)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(572, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Right Click QR Code To Copy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(572, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Fixed Until Broken 2024";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 466);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBoxQRCode);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonGenerate);
+            this.Name = "Form1";
+            this.Text = "QR Gen";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.PictureBox pictureBoxQRCode;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ QRgen/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace QRgen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Add MouseUp event handler for the PictureBox
+            pictureBoxQRCode.MouseUp += pictureBoxQRCode_MouseUp;
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            string inputText = textBoxInput.Text;
+            if (!string.IsNullOrEmpty(inputText))
+            {
+                GenerateQRCode(inputText);
+            }
+            else
+            {
+                MessageBox.Show("Please enter some text to generate a QR code.", "Input Needed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void GenerateQRCode(string text)
+        {
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+                using (QRCode qrCode = new QRCode(qrCodeData))
+                {
+                    Bitmap qrCodeImage = qrCode.GetGraphic(trackBar1.Value);
+                    pictureBoxQRCode.Image = qrCodeImage;
+                }
+            }
+        }
+
+        private void pictureBoxQRCode_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                ContextMenuStrip contextMenu = new ContextMenuStrip();
+                ToolStripMenuItem copyImageItem = new ToolStripMenuItem("Copy Image");
+                copyImageItem.Click += CopyImageItem_Click;
+                contextMenu.Items.Add(copyImageItem);
+                contextMenu.Show(pictureBoxQRCode, e.Location);
+            }
+        }
+
+        private void CopyImageItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxQRCode.Image != null)
+            {
+                Clipboard.SetImage(pictureBoxQRCode.Image);
+                MessageBox.Show("QR code copied to clipboard!", "Copied", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No image to copy.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+  
+    }
+}
